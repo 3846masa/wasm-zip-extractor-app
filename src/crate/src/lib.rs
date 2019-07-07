6f38f835b229a964e4cf240c02c3d57a52cc31b2
3846masa <3846masahiro+git@gmail.com>
2019-07-07 23:29:45 +0900

    Update Rust code

--- src/crate/src/lib.rs
+++ src/crate/src/lib.rs
@@ -1,24 +1,18 @@
-extern crate cfg_if;
 extern crate wasm_bindgen;
 extern crate zip;
 
 mod utils;
 
-use cfg_if::cfg_if;
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 use zip::ZipArchive;
 
-cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
-    if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
 pub fn initialize() {
@@ -35,17 +29,17 @@ impl ZipReader {
     #[wasm_bindgen(constructor)]
     pub fn new(buf: Vec<u8>) -> ZipReader {
         let reader = Cursor::new(buf);
-        let mut zip = ZipArchive::new(reader).unwrap();
+        let zip = ZipArchive::new(reader).unwrap();
         ZipReader { zip }
     }
 
     #[wasm_bindgen(js_name = getFilenameList)]
     pub fn get_filename_list(&mut self) -> Vec<JsValue> {
-        let mut zip = &mut self.zip;
+        let zip = &mut self.zip;
         let mut filename_list: Vec<String> = Vec::new();
 
         for idx in 0..zip.len() {
-            let mut file = zip.by_index(idx).unwrap();
+            let file = zip.by_index(idx).unwrap();
             let name = file.name().to_owned();
             if !name.ends_with("/") {
                 filename_list.push(name);
@@ -57,7 +51,7 @@ impl ZipReader {
 
     #[wasm_bindgen(js_name = getBinary)]
     pub fn get_binary(&mut self, filename: &str) -> Vec<u8> {
-        let mut zip = &mut self.zip;
+        let zip = &mut self.zip;
         let mut file = zip.by_name(filename).unwrap();
         let mut buf = Vec::with_capacity(file.size() as usize);
         std::io::copy(&mut file, &mut buf).unwrap();
@@ -72,7 +66,7 @@ pub fn get_filename_list(buf: Vec<u8>) -> Vec<JsValue> {
     let mut filename_list: Vec<String> = Vec::new();
 
     for idx in 0..zip.len() {
-        let mut file = zip.by_index(idx).unwrap();
+        let file = zip.by_index(idx).unwrap();
         let name = file.name().to_owned();
         if !name.ends_with("/") {
             filename_list.push(name);
